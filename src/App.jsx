e159786a568d688f7ef2984813242e3f7550d66e
ikjames5454 <ikennajames03@gmail.com>
2026-02-13 10:22:38 +0100

    completed the first part of hero section with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 function App() {
   return (
-    <div className='flex-center'>
-      App
-    </div>
+    <main>
+     <Navbar/>
+     <Hero/>
+     <div className='h-dvh' />
+    </main>
   )
 }
 
